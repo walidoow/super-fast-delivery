cf3305813f6f80b01627f22fcaf1b2bcbb56329f
Irisvella <122749049+Irisvella@users.noreply.github.com>
2023-11-28 10:32:49 -0500

    shippinginfo + layout change
    Change layout to allow overflow + style the shippinginfo page.
    Will refine style later.

--- pages/pricing.tsx
+++ pages/pricing.tsx
@@ -1,7 +1,12 @@
 import React, { useState } from 'react';
 import Link from 'next/link';
 
+interface StepCircleProps {
+    filled: boolean;
+    label: string;
+  }
 
+  // The box for enter ItemID + Quantity
 const PackageInput = () => (
   <div className="bg-orange-200 p-4 mb-4">
     <div className="mb-3">
@@ -14,6 +19,30 @@ const PackageInput = () => (
     </div>
   </div>
 );
+
+
+const StepCircle: React.FC<StepCircleProps> = ({ filled, label }) => (
+    <div className="flex flex-col items-center">
+      <div
+        className={`w-8 h-8 rounded-full ${filled ? 'bg-blue-700' : 'bg-white border-2 border-gray-400'} flex items-center justify-center`}
+      >
+        {filled && <div className="w-4 h-4 rounded-full bg-white" />}
+      </div>
+      <span className="text-xs mt-1">{label}</span>
+    </div>
+  );
+  
+  const ProgressBar = () => (
+    <div className="flex items-center mb-6">
+      <StepCircle filled={true} label="Details" />
+      {["Shipping", "Payment", "Review", "Thank You"].map((label, index) => (
+        <React.Fragment key={index}>
+          <div className="flex-1 border-t-2 border-dotted border-black-400 mx-2" />
+          <StepCircle filled={false} label={label} />
+        </React.Fragment>
+      ))}
+    </div>
+  );
 
 const Pricing = () => {
   
@@ -30,6 +59,7 @@ const Pricing = () => {
     <h2 className="text-3xl font-bold mb-6 text-center">Request a Delivery</h2>
       <div className="bg-orange-500 w-full max-w-2xl flex flex-col items-center p-8">
         <h2 className="text-2xl font-bold text-black mb-6">Enter Package Information</h2>
+        <ProgressBar />
         <div className="w-full">
           
           {packageInputs.map(input => (
