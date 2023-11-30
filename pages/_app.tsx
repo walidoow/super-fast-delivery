81c151837f3b262573efa0a3ff84ac1341e4ca53
Irisvella <122749049+Irisvella@users.noreply.github.com>
2023-11-30 12:24:07 -0500

    Added review page (#11)
    review page + font awesome for the clickable stars

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,15 @@
 import { AppProps } from "next/app";
 import Layout from "@/components/layouts/layout";
 import { Inter } from "next/font/google";
+import Head from 'next/head';
 
 const inter = Inter({subsets: ['latin']})
 
 const App = ({Component, pageProps}: AppProps) => (
-  <>
+  <><link
+  href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
+  rel="stylesheet"
+/>
     <style jsx global>{`
       html {
         font-family: ${inter.style.fontFamily};
