51fef38b7faf0e968d1786991da318c682fab99b
Inas Fawzi <95307617+inas-fawzi@users.noreply.github.com>
2023-11-30 10:12:46 -0500

    Create quotation.tsx
    adding the page for quotation

---
+++ pages/quotation.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { CardQuotation } from '@/components/CardQuotation'
+//import 'bootstrap/dist/css/bootstrap.css'
+
+const quotation = () => {
+    return (
+        <body className="bg-[#EDEEC9]">
+            <div className = "flex justify-center pt-11 pb-11">
+                <CardQuotation />
+            </div>
+        </body>
+    )
+}
+
+export default quotation
